88afc578206cec3ac2f1d2a06e1668690d4d0102
lucassherwin <lksherwin@gmail.com>
2020-12-02 14:39:43 -0500

    added writing output to file

--- day_2/day2.py
+++ day_2/day2.py
@@ -8,6 +8,9 @@ def day2(input):
     # count to track the number of valid passwords
     count = 0
 
+    #write solution to file
+    output = open('output.txt', 'w')
+
     for i in input:
         # arr[0] is the policy and arr[1] is the password
         arr = i.replace(" ", "").split(':')
@@ -27,6 +30,9 @@ def day2(input):
         # check if letter appears in password at least 'lower' times and at most 'upper' times
         if(password.count(letter) <= upper and password.count(letter) >= lower):
             count += 1
+            output.write(f'{password} is valid\n')
+    output.write(f'There are {count} valid passwords')
+    output.close()
     print(count)
     return count
 
