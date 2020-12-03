91befd914a878ff72c48bc5e6a5f78838565ef44
lucassherwin <lksherwin@gmail.com>
2020-12-03 15:10:49 -0500

    updated the way the file is read in

--- day_3/day3.py
+++ day_3/day3.py
@@ -1,6 +1,5 @@
-with open('input.txt', 'r') as file:
-    input_lines = [line.strip() for line in file]
-    # print(input_lines)
+input_txt = open('input.txt', 'r')
+lines = input_txt.readlines()
 
 # count all trees('#') you would encounter going right 3 down 1
 def count_trees(input):
@@ -16,17 +15,14 @@ def count_trees(input):
         # check if idx + 3 will be greater than len(i)
         if((idx+3) > (len(i)-1)):
             # if so, set idx to be the length - idx
-            idx = (len(i)-1)-idx
+            idx = (len(i))-idx
         # otherwise increment idx by 3
         else:
             idx += 3
+        print(idx, len(i), i[idx], count)
     print(count)
     return count
 
 
 
-    print(count)
-    return count
-
-
-count_trees(input_lines)
+count_trees(lines)
