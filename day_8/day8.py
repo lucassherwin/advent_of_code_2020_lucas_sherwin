be2303255a12ad88b4a2705b9d0d1410a7f0ab2c
lucassherwin <lksherwin@gmail.com>
2020-12-08 23:38:17 -0500

    part 2 works

--- day_8/day8.py
+++ day_8/day8.py
@@ -12,10 +12,12 @@ def find_bootloop(input):
     i = 0
     while i < len(input):
         print(input[i])
-
+        
+        # loop found
         if i in executed:
             print('acc = ', acc)
-            return acc
+            # false means it still is in a loop
+            return False
         # get the instruction to be executed
         inst = input[i].split()
         # seperate operation and argument
@@ -32,7 +34,49 @@ def find_bootloop(input):
             print('how did i get here', i)
         executed.add(i)
         i += 1
+    # no boot loop found
     print('acc = ', acc)
     return acc
 
-find_bootloop(input_lines)
+# finds bad instruction
+def find_bad_inst(input):
+    # copy the input to test
+    copy = input.copy()
+
+    # loop over the input 
+    for i in range(len(copy)):
+        # check if the instruction is nop
+        if 'nop' in copy[i]:
+            # if so swap nop and jmp
+            copy[i] = copy[i].replace('nop', 'jmp')
+            # test it
+            test = find_bootloop(copy)
+            # if find_bootloop returns true
+            if test is not False:
+                # find_bootloop() will return acc if it is not found
+                print('acc = ', test)
+                # break out of the loop
+                break
+            # find_bootloop returns false
+            else:
+                # bootloop was found
+                continue
+        elif 'jmp' in copy[i]:
+            # replace jmp with nop
+            copy[i] = copy[i].replace('jmp', 'nop')
+            # test
+            test = find_bootloop(copy)
+            # print('elif jmp', 'test', test, 'i', i)
+            # if its not false
+            if test is not False:
+                # no loop found
+                print('acc = ', test)
+                break
+            # reset the input and continue
+        copy = input.copy()
+
+
+find_bad_inst(input_lines)
+# incorrect: 1744 (too high)
+
+# find_bootloop(input_lines)
