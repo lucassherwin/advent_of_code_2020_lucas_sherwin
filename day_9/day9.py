ca3404d4f271c274ccc1bbcd09fab5fdd1d0a153
lucassherwin <lksherwin@gmail.com>
2020-12-09 22:41:55 -0500

    fixing part 1

---
+++ day_9/day9.py
@@ -0,0 +1,42 @@
+with open('input.txt', 'r') as file:
+    input_lines = file.readlines()
+
+# find the first number in the list (after preamble) which is not the sum of two of the 25 numbers before it
+# the number immediately after the preamble will be the sum of any 2 numbers in the preamble 
+
+# STEPS:
+    # 1. get preamble (starts as 0-24)
+    # check if [25] is the sum of any 2 numbers in preamble
+    # if so:
+        # increase preamble start and end by 1 and check next number
+    # otherwise:
+        # return the bad number
+
+def find_num(input):
+    # preamble start and end
+    # start = 0
+    # end = 24
+    pre = input[0:24]
+
+    # the number immediately after the preamble
+    check_num = input[25]
+    for i in range(len(pre)-1):
+        j = i + 1
+        
+        # check if i + j == check_num
+        # indicating that the number is good
+        if i + j == check_num:
+            input = input[1:len(input)]
+
+            # call the function with new input
+            find_num(input)
+        # found the bad number
+        elif i == len(pre):
+            print('bad: ', check_num)
+    return check_num
+    print('asdf')
+
+
+
+
+find_num(input_lines)
