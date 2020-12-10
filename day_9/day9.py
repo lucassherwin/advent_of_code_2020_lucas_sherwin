40b4e5b0d89eb626e05e6224d21a0d32fb28bb21
lucassherwin <lksherwin@gmail.com>
2020-12-10 00:08:07 -0500

    part 1 solved

--- day_9/day9.py
+++ day_9/day9.py
@@ -1,4 +1,4 @@
-with open('example.txt', 'r') as file:
+with open('input.txt', 'r') as file:
     input_lines = [int(line.strip()) for line in file]
 
 # find the first number in the list (after preamble) which is not the sum of two of the 25 numbers before it
@@ -16,63 +16,53 @@ def find_num(input):
     # preamble start and end
     # start = 0
     # end = 24
-    pre = input[0:4]
+    pre = input[0:25]
 
     # the number immediately after the preamble
     # print(input)
-    check_num = input[5]
+    check_num = input[25]
 
 
 
     i = 0
-    while i <= len(pre):
+    while i < len(pre):
         for j in pre:
             # if i and j are the same
-            print('i', i, 'j', j, 'pre[i]', pre[i], "check_num", check_num)
+            # print( 'pre[i]', pre[i], "check_num", check_num)
+            # print('new -----------------')
+            # print(input)
+            # print(pre)
+            # print('i', i, 'j', j, 'check_num', check_num)
+            # print(pre[i])
+            # print('-----------------')
+
             if pre[i] == j:
-                print('skip!')
-                print(input)
+                # print('skip!')
+                # print(input)
                 continue
             # check if i + j = check_num
             elif pre[i] + j == check_num:
                 # if so the number is good 
                 # call the function recursively
-                print('pre[i]', pre[i], 'j', j, 'check_num', check_num)
+                # print('pre[i]', pre[i], 'j', j, 'check_num', check_num)
                 input = input[1 : len(input)]
-                print(input)
-                print('call again')
+                # print(input)
+                # print('call again')
                 find_num(input)
             # no match is found meaning the number is bad
             elif i+1 > len(pre):
-                print(input)
+                # print(input)
                 print('found: ', check_num)
                 return check_num
         i += 1
     print('bad: ', check_num)
-    return check_num
-
-
-
-
-
-
-
-    # for i in range(len(pre)-1):       
-    #     # check if i + j == check_num
-    #     # indicating that the number is good
-    #     if pre[i] + pre[j] == check_num:
-    #         input = input[1:len(input)]
-
-    #         # call the function with new input
-    #         find_num(input)
-    #     # found the bad number
-    #     elif i == len(pre):
-    #         print('bad: ', check_num)
-    # # print('asdf')
-    # print(check_num)
+    raise Exception('bad: ', check_num)
     # return check_num
 
 
+try:
+    find_num(input_lines)
+except:
+    pass
 
-
-find_num(input_lines)
+# incorrect: 4
