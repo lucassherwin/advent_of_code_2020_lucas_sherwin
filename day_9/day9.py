6b28dbcd67299aea32d95447edd96a00b4a873e1
lucassherwin <lksherwin@gmail.com>
2020-12-09 22:48:28 -0500

    changes

--- day_9/day9.py
+++ day_9/day9.py
@@ -1,4 +1,4 @@
-with open('input.txt', 'r') as file:
+with open('example.txt', 'r') as file:
     input_lines = file.readlines()
 
 # find the first number in the list (after preamble) which is not the sum of two of the 25 numbers before it
@@ -16,24 +16,25 @@ def find_num(input):
     # preamble start and end
     # start = 0
     # end = 24
-    pre = input[0:24]
+    pre = input[0:4]
 
     # the number immediately after the preamble
-    check_num = input[25]
+    check_num = input[5]
     for i in range(len(pre)-1):
         j = i + 1
         
+        
         # check if i + j == check_num
         # indicating that the number is good
-        if i + j == check_num:
+        if pre[i] + pre[j] == check_num:
             input = input[1:len(input)]
 
             # call the function with new input
             find_num(input)
         # found the bad number
-        elif i == len(pre):
+        elif pre[i] == len(pre):
             print('bad: ', check_num)
-    print('asdf')
+    # print('asdf')
     return check_num
 
 
